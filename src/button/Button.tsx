dec432d4237ded932c1509486fff3bc57e771bf8
beodwilson <beodwilson@gmail.co>
2023-07-02 21:32:12 +0000

    refactor button component to include links.

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -2,33 +2,48 @@ import React, { ForwardedRef } from 'react'
 import clsx from 'clsx'
 import styles from './Button.module.css'
 
-interface ButtonProps extends React.ComponentProps<'button'> {
+type buttonType = React.ComponentProps<'button'> & React.ComponentProps<'a'>
+interface ButtonProps extends buttonType {
   danger?: Boolean
   outlined?: boolean
 }
 
 const Button = React.forwardRef(
   (
-    { children, className, outlined, danger, ...forwardedProps }: ButtonProps,
+    { children, className, outlined, href, ...forwardedProps }: ButtonProps,
     ref: ForwardedRef<HTMLButtonElement> | undefined
   ) => {
-    return (
-      <button
-        role="button"
-        {...forwardedProps}
-        className={clsx(
-          styles.button,
-          {
-            [styles.buttonOutlined]: outlined,
-            [styles.buttonDanger]: danger,
-          },
-          className
-        )}
-        ref={ref}
-      >
-        {children}
-      </button>
+    //Check if elem has href and render html element  accoordingly.
+    const elementType = href ? 'a' : 'button'
+    const isDisabledLink = elementType === 'a' && forwardedProps.disabled
+    // Intercept forwarded onClick function
+    const forwardedOnClick = forwardedProps.onClick
+    let handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      if (isDisabledLink) {
+        e.preventDefault()
+        return
+      }
+      forwardedOnClick && forwardedOnClick(e)
+    }
+
+    className = clsx(
+      styles.button,
+      {
+        [styles.buttonOutlined]: outlined,
+      },
+      `${className} ${isDisabledLink ? styles.noCursorDisabledForLinks : ''}`
     )
+    let onClick = handleClick
+    let button = React.createElement(elementType, {
+      children,
+      className,
+      outlined,
+      href,
+      ...forwardedProps,
+      onClick,
+      ref,
+    })
+    return button
   }
 )
 
